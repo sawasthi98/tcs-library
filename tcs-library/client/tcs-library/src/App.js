4293c53bc540531222b24b9a1b032678d3d6b554
ChiomaNnadi <bnnadi123@gmail.com>
2023-08-15 14:49:32 -0400

    something in the front end

---
+++ tcs-library/client/tcs-library/src/App.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import './App.css';
+import Landing from './components/Landing';
+
+function App() {
+
+  const [user, setUser] = useState(null);
+
+}
+
+export default App;
